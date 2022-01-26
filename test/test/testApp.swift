22800621953c9c81828e670e609cd7e6e27f3138
yqaiosmail01 <yqaiosmail01@gmail.com>
2022-01-26 19:54:26 +0000

    Initial commit

---
+++ test/test/testApp.swift
@@ -0,0 +1,17 @@
+//
+//  testApp.swift
+//  test
+//
+//  Created by ec2-user on 1/25/22.
+//
+
+import SwiftUI
+
+@main
+struct testApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
